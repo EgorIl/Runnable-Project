cea7cdbba016157f97c965fea829a935f53abf9f
EgorIl <jt.marlin.ltd@gmail.com>
2016-05-12 22:34:03 +0600

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,11 +1,11 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind(('127.0.0.1', 2222)) 
+s.bind(('0.0.0.0', 2222)) 
 s.listen(1) 
 while True:
         conn, addr = s.accept()
         while True:
                 data = conn.recv(2222)
-                if not data : break
+                if data == b'close'
                 conn.send(data)
         conn.close()
